0b121d0497dff92dd5d5830c964b6e057961cb6e
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-05-12 10:05:04 +0200

    Example swipe cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,188 +1,137 @@
+'use strict';
+
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
-import { MonoText } from '../components/StyledText';
+import SwipeCards from 'react-native-swipe-cards';
 
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              RUCHAM PSA JAK SRA xD
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
     }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+    render() {
+        return (
+            <View style={styles.card}>
+                <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+                <Text style={styles.text}>This is card {this.props.name}</Text>
+            </View>
+        )
+    }
+}
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+class NoMoreCards extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.noMoreCards}>
+                <Text>No more cards</Text>
+            </View>
+        )
+    }
+}
+
+const cards = [
+    {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+    {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+    {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+    {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+    {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+    {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+    {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+    {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+    {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+
+const cards2 = [
+    {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+    {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+    {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+    {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: cards,
+            outOfCards: false
+        }
+    }
+
+    handleYup (card) {
+        console.log("yup")
+    }
+
+    handleNope (card) {
+        console.log("nope")
+    }
+
+    cardRemoved (index) {
+        console.log(`The index is ${index}`);
+
+        let CARD_REFRESH_LIMIT = 3
+
+        if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+            console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+
+            if (!this.state.outOfCards) {
+                console.log(`Adding ${cards2.length} more cards`)
+
+                this.setState({
+                    cards: this.state.cards.concat(cards2),
+                    outOfCards: true
+                })
+            }
+
+        }
+
+    }
+
+    render() {
+        return (
+            <SwipeCards
+                cards={this.state.cards}
+                loop={false}
+
+                renderCard={(cardData) => <Card {...cardData} />}
+                renderNoMoreCards={() => <NoMoreCards />}
+                showYup={true}
+                showNope={true}
+
+                handleYup={this.handleYup}
+                handleNope={this.handleNope}
+                cardRemoved={this.cardRemoved.bind(this)}
+            />
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+    card: {
+        alignItems: 'center',
+        borderRadius: 5,
+        overflow: 'hidden',
+        borderColor: 'grey',
+        backgroundColor: 'white',
+        borderWidth: 1,
+        elevation: 1,
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+    },
+    text: {
+        fontSize: 20,
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    noMoreCards: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
